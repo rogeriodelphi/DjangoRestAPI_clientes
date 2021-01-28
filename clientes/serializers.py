ecb0ee2b616e8eb1de3ed06bf730f38a73608736
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-27 23:27:19 -0300

    Importando validações - CPF

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -10,9 +10,11 @@ class ClienteSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos."})
+            raise serializers.ValidationError({'cpf': "Número de CPF inválido!"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': "Não inclua números neste campo."})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg': "O RG deve ter 9 dígitos."})
-
+        if celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': "O número do celular deve seguir este modelo: 99 99999-9999."})
+        return data
